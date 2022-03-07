921ec88bfb766c8eacec9a31388ea5cc4369cc6c
jan.ptacek <jan.ptacek@VT1-16>
2022-03-07 13:17:29 +0100

    Přidejte soubory projektu.

---
+++ for/Program.cs
@@ -0,0 +1,15 @@
+﻿Dictionary<string, string> ahoj = new Dictionary<string, string>();
+Console.WriteLine("zadej velikost slovniku");
+int b = Convert.ToInt32(Console.ReadLine());    
+for  (int a= 0;a<b; a++)
+{
+    Console.WriteLine("napis jmeno");
+    string? jmeno= Console.ReadLine();
+    Console.WriteLine("napis cislo");
+    string? cislo= Console.ReadLine();
+    ahoj.Add(jmeno, cislo);
+}
+foreach (var řádek in ahoj)
+{
+    Console.WriteLine($"klíč: {řádek.Key} hodnota: {řádek.Value}");
+}
